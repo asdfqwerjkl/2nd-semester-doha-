f4629b84562681ac7b7a1ca040f652b08457b5e9
asdfqwerjkl <92816515+asdfqwerjkl@users.noreply.github.com>
2021-10-20 03:04:18 +0900

    Add files via upload

---
+++ 1.2.py
@@ -0,0 +1,29 @@
+x = 7
+y = 6
+
+print(x+y)
+
+x = '7'
+y = '6'
+print(x+y)
+
+import turtle
+t = turtle.Turtle()
+t.shape("turtle");
+
+radius = 50
+x = 30
+t.circle(radius)
+t.fd(x)
+t.circle(radius)
+x=50
+t.fd(x)
+t.circle(radius)
+
+x = int(input("첫 번째 정수를 입력하시오: "))
+y = int(input("두 번째 정수를 입력하시오: "))
+
+print(x, "+",y,"=", x+y)
+print(x, "-",y,"=", x-y)
+print(x, "*",y,"=", x*y)
+print(x, "/",y,"=", x/y)
