803e411e0d21e8af8afd1ef6a9c0305e55e8b8f1
asdfqwerjkl <92816515+asdfqwerjkl@users.noreply.github.com>
2021-10-24 19:13:15 +0900

    Add files via upload

---
+++ practice1.py
@@ -0,0 +1,118 @@
+print("환영합니다.")
+print("파이썬의 세계에 오신 것을 환영합니다.")
+print("파이썬은 강력합니다.")
+
+import turtle           
+t = turtle.Turtle()
+t.shape("turtle");   
+t.forward(100);         
+t.left(90) 
+t.forward(100);         
+t.left(90) 
+t.forward(100);         
+t.left(90) 
+t.forward(100);         
+t.left(90) 
+t.forward(100);         
+exitonclick()       #화면을 마우스로 클릭해야 종료되게 하는 부분
+
+from turtle import 
+shape("turtle")
+t.forward(100);         
+t.left(90) 
+t.forward(100);         
+t.left(90) 
+t.forward(100);         
+t.left(90) 
+t.forward(100);         
+t.left(90) 
+t.forward(100); 
+write("종료하시려면 화면클릭")
+exitonclick()       #화면을 마우스로 클릭해야 종료되게 하는 부분
+
+
+import turtle           
+t = turtle.Turtle()
+t.shape("turtle"); 
+t.width(10)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t._screen.exitonclick()      #화면을 마우스로 클릭해야 종료되게 하는 부분
+
+import turtle           
+t = turtle.Turtle()
+t.shape("turtle"); 
+t.color(blue)
+t.forward(100)
+t._screen.exitonclick()      #화면을 마우스로 클릭해야 종료되게 하는 부분
+
+import turtle           
+t = turtle.Turtle()
+t.shape("turtle"); 
+t.shape("square")
+import turtle           
+t = turtle.Turtle()
+t.forward(100)
+t._screen.exitonclick()      #화면을 마우스로 클릭해야 종료되게 하는 부분
+
+import turtle           
+t = turtle.Turtle()
+t.shape("turtle"); 
+t.up()
+t.goto(0,0)
+t.down()
+t.forward(100);
+
+t.up()
+t.goto(0,100)
+t.down()
+t.forward(100);
+
+t._screen.exitonclick()      #화면을 마우스로 클릭해야 종료되게 하는 부분
+
+from turtle import
+shape("turtle")
+shape("arrow")
+up()
+goto(0,0)
+down()
+forward(100)
+up()
+goto(0,100)
+down()
+forward(100)
+exitonclick() 
+
+import turtle           
+t = turtle.Turtle()
+t.shape("turtle"); 
+
+t.up(); t.goto(-150,0); t.down();t.circle(80)
+t.up(); t.goto(0,0); t.down();t.circle(80)
+t.up(); t.goto(150,0); t.down();t.circle(80)
+t.up(); t.goto(-80,-100); t.down();t.circle(80)
+t.up(); t.goto(80,-100); t.down();t.circle(80)
+t._screen.exitonclick()      #화면을 마우스로 클릭해야 종료되게 하는 부분
+
+import turtle           
+t = turtle.Turtle()
+t.shape("turtle"); 
+
+t.up();t.width(5);t.color("blue") t.goto(-150,0); t.down();t.circle(80)
+t.up();t.width(5);t.color("black") t.goto(-30,0); t.down();t.circle(80)
+t.up();t.width(5);t.color("red") t.goto(100,0); t.down();t.circle(80)
+t.up();t.width(5);t.color("yellow") t.goto(-90,-90); t.down();t.circle(80)
+t.up();t.width(5);t.color("green") t.goto(40,90); t.down();t.circle(80)
+t.write("종료하려면화면클릭")
+t._screen.exitonclick()      #화면을 마우스로 클릭해야 종료되게 하는 부분
+
+from turtle import
+shape("turtle")
+up();width(5);color("blue") ;goto(-150,0); down();circle(80)
+up();width(5);color("black") ;goto(-30,0); down();circle(80)
+up();width(5);color("red") ;goto(100,0); down();circle(80)
+up();width(5);color("yellow") ;goto(-90,-90); down();circle(80)
+up();width(5);color("green");goto(40,-90); down();circle(80)
+write("종료하려면 화면클릭")                    
+exitonclick()      #화면을 마우스로 클릭해야 종료되게 하는 부분
