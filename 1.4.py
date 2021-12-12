12f96f4174a0026fbadecb0d9702d6a218bc34fb
asdfqwerjkl <92816515+asdfqwerjkl@users.noreply.github.com>
2021-12-12 17:23:13 +0900

    Update 1.4.py

--- 1.4.py
+++ 1.4.py
@@ -33,6 +33,6 @@ import time
 now = time.time()
 thisYear = int(1970 + now//(365*24*3600))
 
-print("올해는",thisyear,"입니다.")
+print("올해는",thisYear,"입니다.")
 age = int(input("몇 살이신지요?"))
-print("2050년에는",(age+2050-thisyear,"살 이시군요"))
+print("2050년에는",(age+2050-thisYear,"살 이시군요"))
