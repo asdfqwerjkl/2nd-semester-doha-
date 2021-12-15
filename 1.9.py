465b7797d1c9f57b9adc3fcbed7aa65da8323ceb
asdfqwerjkl <92816515+asdfqwerjkl@users.noreply.github.com>
2021-12-15 13:56:57 +0900

    Add files via upload

--- 1.9.py
+++ 1.9.py
@@ -1,60 +1,37 @@
-import random           #랜덤모듈을 불러온다.
-question = ["56 / 8", "8 * 9", "50 - 25", "1 + 6", "81 / 9",
- "22 + 10", "8 / 4", "9 * 7", "17 - 4", "3 + 5"]
-dailyQuestion = random.choice(question)     #dailyQuestion변수의 숫자 
-print("#########################")          #출력
-print("# 오늘의 산수 문제 #")                #출력
-print("#########################")          #출력
-print("")                                   #출력
-print(dailyQuestion)                        #출력
-
-
-items = {"커피음료": 7, "펜": 3, "종이컵": 2, "우유": 1, "콜라": 4, "책": 5}
-while True:     #반복문
-    print("")   #출력
-    print("# 재고 목록 #")              #출력
-    for key in sorted(items.keys()):   #반복문     
-    print(key, items[key])              #출력
-    print("\n********************")     #출력
-    print("0. 종료")                    #출력
-    print("1. 재고 추가")               #출력
-    print("2. 재고 삭제")               #출력
-    print("********************\n")     #출력
-    a = int(input("무엇을 하시겠습니까?: "))    #a의 변수 입력
- 
-    if a == 1:                      #가정문
-        item = input("물건의 이름을 입력하시오: ")      #item의 변수 입력
-        num = input("몇개를 추가하시겠습니까? :")       #num의 변수 입력
-        items[item] = int(items[item]) + int(num)     
-    elif a == 2:                    #가정문
-        item = input("물건의 이름을 입력하시오: ")       #item의 변수 입력
-        num = input("몇개를 삭제하시겠습니까? :")       #num의 변수 입력
-        items[item] = int(items[item]) - int(num)
- 
-    else:                   #가정문
-        break               #루프밖으로 분기
-
-english_dict = dict()           #사전식
-english_dict['하나'] = 'one'            #하나면 one
-english_dict['둘'] = 'two'              #둘이면 two
-english_dict['셋'] = 'three'            #셋이면 three
-word = input("단어를 입력하시오: ")     #입력
-print (english_dict[word])          #출력
-
-import smtplib              #SMTP을 사용하기 위한 모듈
-from email.mime.text import MIMEText    #메일을 보낼때 메시지의 제목과 본문을 설정하기 위한 모듈
-me = 'abc@server.kr' # 보내는 사람 메일 주소
-you = ['def1@server.com','def2@server.com','def3@server.com'] # 받는 사람 메일 주소
-contents = '12월 20일에 동창회가 있으니 참석해주시기 바랍니다.'     #contents변수
-msg = MIMEText(contents, _charset='euc-kr')         
-msg['Subject'] = '동창회 모임'  #메일의 제못을 지정해준다
-msg['From'] = me                #보내는 사람을 지정해준다
-msg['To'] = you                 #받는 사람을 지정해준다
-server = smtplib.SMTP('smtp.gmail.com', 587)    #지메일의 포트 번호  = 587
-server.ehlo()               #SMTP서버에 메시지를 보내주는 메소드
-server.starttls()           #TLS보안 시작
-server.ehlo()
-server.login("자신의 아이디", "패스워드")   #로그인을 인증(지메일계정, 앱 비밀번호)
-for i in range(len(you)):           #반복문
-    server.sendmail(me, you[i], msg.as_string())
-server.quit()       #STMP서어봐의 연결종료
+import turtle # 터틀 그래픽 모듈을 불러온다.
+import random # 난수 모듈을 불러온다.
+screen = turtle.Screen()
+image1 ="D:/rabbit.gif"     #토끼이미지
+image2 ="D:/turtle.gif"     #거북이1이미지
+image3 ="D:/turtle2.gif"    #거북이2이미지
+screen.addshape(image1)     #이미지추가(토끼)
+screen.addshape(image2)     #이미지추가(거북이1)
+screen.addshape(image3)     #이미지추가(거북이2)
+t1 = turtle.Turtle() # 첫 번째 거북이를 생성한다. 
+t1.shape(image1)     #t1을 이미지 1로 표현한다
+t1.pensize(5) # 펜의 두께를 5로 한다. 
+t1.penup() # 펜을 든다. 
+t1.goto(-300, 0) # (-300, 0) 위치로 간다.
+t2 = turtle.Turtle() # 두 번째 거북이를 생성한다. 
+t2.shape(image2)    #t2을 이미지 2로 표현한다
+t2.pensize(5) # 펜의 두께를 5로 한다. 
+t2.penup() # 펜을 든다. 
+t2.goto(-300, -100) # (-300, -100) 위치로 간다.
+t3 = turtle.Turtle() # 세 번째 거북이를 생성한다. 
+t3.shape(image2)    #t3을 이미지 3로 표현한다
+t3.pensize(5) # 펜의 두께를 5로 한다. 
+t3.penup() # 펜을 든다. 
+t3.goto(-300, -200) # (-300, -200) 위치로 간다.
+t1.pendown() # 첫 번째 거북이의 펜을 내린다. 
+t2.pendown() # 두 번째 거북이의 펜을 내린다. 
+t3.pendown() # 세 번째 거북이의 펜을 내린다. 
+t1.speed(1)
+t2.speed(1) 
+t3.speed(1)
+for i in range(100): # 100번 반복한다.
+    d1 = random.randint(1, 60) # 1부터 60 사이의 난수를 발생한다.
+    t1.forward(d1) # 난수만큼 이동한다.
+    d2 = random.randint(1, 60)# 1부터 60 사이의 난수를 발생한다.
+    t2.forward(d2)# 난수만큼 이동한다.
+    d3 = random.randint(1, 60)# 1부터 60 사이의 난수를 발생한다.
+    t3.forward(d3)# 난수만큼 이동한다.
